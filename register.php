66118b0e2c0eb8017af01d133012df8c8944f26c
mandeep35 <55003442+mandeep35@users.noreply.github.com>
2019-12-04 15:10:26 -0700

    Update register.php

--- register.php
+++ register.php
@@ -85,7 +85,7 @@ http://uncevents.online/verify.php?verify=$verify_string";
 			<input type="password" placeholder="Confirm Password" name="verify" required><br>
 			<hr>
 			<input type="checkbox" name="is_faculty"> I am Faculty <br>
-			<button type="submit">Register</button>
+			<button type="submit">Register</button><br>
     	</body>
  	</div>
 </form>
