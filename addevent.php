231867af4fcfe9b825270a828d62c0bc4dc0ab6d
Casey Burklow <casey.burklow@unco.edu>
2019-12-04 11:12:43 -0700

    [event] Add tags to add,edit event

--- addevent.php
+++ addevent.php
@@ -53,7 +53,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST))
 			$stmt->execute(array(':event_id' => $row['m'], ':tag_id' => $tag_id));
 		}
 	}
-
+	// ADD AUTO-EMAIL HERE
+	$stmt = $db -> prepare (
+	"SELECT user.email as email, event.event_name
+	 FROM user
+	LEFT JOIN eventfollower ON eventfollower.user_id = user.user_id
+	WHERE event.event_id = :id"
+	);
+	$stmt -> execute(array(":id" => $_POST["id"])); // Assuming that it posts to self with ID as a parameter
+	while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) // Get associative array
+	{
+		$email = $row['email'];
+		$event = $row['event_name'];
+		$message = 'An event has been updated
+		Chech out the changes at https://uncevents.online/event.php?id='.$_POST['id'];
+		emailNotifaction($message, $event, $email, $noreply_email_addr);
+	}
   echo '<p class="success">Event created.</p>';
 	header("Location: event.php?id=".$row['m']);
 }
@@ -102,18 +117,18 @@ $(function() {
 	<p>Does this event have food? <input type="checkbox" name="has_food">
 	</p><br>
 	<!-- dropdown menu to assign it a tag (for searching) -->
-	  Tags:<br>
-		<select data-placeholder="Begin typing to filter tags..." multiple class="chosen-select" name="tags[]">
-			<option value=""></option>
-			<?php
-			$stmt = $db->prepare("SELECT tag_id, tag FROM tag ORDER BY tag_id");
-			$stmt->execute();
-			while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-			{
-				echo '<option value="' . $row['tag_id'] . '">' . $row['tag'] . '</option>';
-			}
-			?>
-		</select>
+  Tags:<br>
+	<select data-placeholder="Begin typing to filter tags..." multiple class="chosen-select" name="tags[]">
+		<option value=""></option>
+		<?php
+		$stmt = $db->prepare("SELECT tag_id, tag FROM tag ORDER BY tag_id");
+		$stmt->execute();
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+		{
+			echo '<option value="' . $row['tag_id'] . '">' . $row['tag'] . '</option>';
+		}
+		?>
+	</select>
 	  <br><br>
 
 <!-- actual file upload for the item itself -->
