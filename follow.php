5e26a7d680170a760cf86b796fc0bb6746e2deef
Casey Burklow <casey.burklow@unco.edu>
2019-12-04 09:20:03 -0700

    [tag] Add tag page, enable following tags, closes #63

--- follow.php
+++ follow.php
@@ -3,17 +3,36 @@
 	$stmt->execute(array(':username' => $_SESSION['username']));
 	$row = $stmt->fetch(PDO::FETCH_ASSOC);
 	$user_id = $row['user_id'];
-	$stmt = $db->prepare("SELECT * FROM eventuser WHERE user_id = :user_id AND event_id = :event_id");
-	$stmt->execute(array(':user_id' => $user_id, ':event_id' => $_POST['event_id']));
-	if ($stmt->rowCount() > 0)
+  if (isset($_POST['event_id']))
 	{
-		$stmt = $db->prepare("DELETE FROM eventuser WHERE user_id = :user_id AND event_id = :event_id");
+		$stmt = $db->prepare("SELECT * FROM eventuser WHERE user_id = :user_id AND event_id = :event_id");
 		$stmt->execute(array(':user_id' => $user_id, ':event_id' => $_POST['event_id']));
+		if ($stmt->rowCount() > 0)
+		{
+			$stmt = $db->prepare("DELETE FROM eventuser WHERE user_id = :user_id AND event_id = :event_id");
+			$stmt->execute(array(':user_id' => $user_id, ':event_id' => $_POST['event_id']));
+		}
+		else
+		{
+			$stmt = $db->prepare("INSERT INTO eventuser (user_id, event_id) VALUES (:user_id, :event_id)");
+			$stmt->execute(array(':user_id' => $user_id, ':event_id' => $_POST['event_id']));
+		}
+		header("Location: event.php?id=" . $_POST['event_id']);
 	}
-	else
+	else if (isset($_POST['tag_id']))
 	{
-		$stmt = $db->prepare("INSERT INTO eventuser (user_id, event_id) VALUES (:user_id, :event_id)");
-		$stmt->execute(array(':user_id' => $user_id, ':event_id' => $_POST['event_id']));
+		$stmt = $db->prepare("SELECT * FROM usertag WHERE user_id = :user_id AND tag_id = :tag_id");
+		$stmt->execute(array(':user_id' => $user_id, ':tag_id' => $_POST['tag_id']));
+		if ($stmt->rowCount() > 0)
+		{
+			$stmt = $db->prepare("DELETE FROM usertag WHERE user_id = :user_id AND tag_id = :tag_id");
+			$stmt->execute(array(':user_id' => $user_id, ':tag_id' => $_POST['tag_id']));
+		}
+		else
+		{
+			$stmt = $db->prepare("INSERT INTO usertag (user_id, tag_id) VALUES (:user_id, :tag_id)");
+			$stmt->execute(array(':user_id' => $user_id, ':tag_id' => $_POST['tag_id']));
+		}
+		header("Location: tag.php?id=" . $_POST['tag_id']);
 	}
-	header("Location: event.php?id=" . $_POST['event_id']);
  ?>
---
+++ tag.php
@@ -0,0 +1,55 @@
+<?php require_once('includes/config.php');
+	// Fetch event
+  $currentID = $_GET["id"];
+	$stmt = $db->prepare("
+   SELECT
+	   tag.tag_id,
+		 tag.tag
+	 FROM tag
+	 WHERE tag.tag_id = :id
+  ");
+	$stmt->execute(array(':id' => $currentID));
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	$item = array(
+    "tag" => $row["tag"],
+		"id" => $row["tag_id"]
+  );
+  $stmt = $db -> prepare("
+  SELECT user_id
+    FROM user
+   WHERE username = :username
+  ");
+	// Check event ownership
+  $stmt -> execute(array(':username' => $_SESSION['username']));
+  $row = $stmt -> fetch(PDO::FETCH_ASSOC);
+  $userID = $row['user_id'];
+	// Get notification status
+	$notif_button_text = "";
+	$stmt = $db->prepare("SELECT * FROM usertag WHERE user_id = :user_id AND tag_id = :tag_id");
+	$stmt->execute(array(':user_id' => $userID, ':tag_id' => $_GET['id']));
+	if ($stmt->rowCount() > 0)
+	{
+		$notif_button_text = "Unfollow";
+	}
+	else
+	{
+		$notif_button_text = "Follow";
+	}
+	$title = $item['tag'];
+	require('layout/header.php');
+?>
+  <br><br>
+    <div class="card">
+      <h1 class="name"><?php echo $item['tag'] ?></h1>
+			<br /><br />
+			<?php
+			if ($user->is_logged_in())
+			{
+				echo '
+			<form action="follow.php" method="post">
+				<input type="hidden" name="tag_id" value="' . $currentID . '">
+				<input type="submit" value="' . $notif_button_text . '">
+			</form>';
+		  } ?>
+    </div>
+<?php require('layout/footer.php') ?>
