8042267cd953812bd52488787b41cda4127d492b
Robodav <dvandiver64@gmail.com>
2019-12-04 10:56:17 -0700

    Give club.php some jesus

--- layout/footer.php
+++ layout/footer.php
@@ -1,8 +1,12 @@
-<!-- <div id="copy">
-	<h1 style="font-size: 32pt;">About UNC Events online</h1><br>
-	<p>UNC Events Online is a central hub for students to host and find events on and near campus, including whether or not they have free food</p><br>
-<a href="https://www.unco.edu/research/office-of-sponsored-programs/policies-procedures-and-forms/">Club/Event Policies</a>
-</div> -->
-
-</body>
-</html>
+<!-- <!DOCTYPE html>
+<html lang="en">
+	<link rel="stylesheet" href="layout\style.css">
+	<body id="footer">
+		<div id="copy">
+			<hr>
+			<h1 id="footer-heading">About UNC Events online</h1>
+			<p>UNC Events Online is a central hub for students to host and find events on and near campus.</p>
+			<a href="https://www.unco.edu/research/office-of-sponsored-programs/policies-procedures-and-forms/">Club/Event Policies</a>
+		</div>
+	</body>
+</html> -->
