3f311ad3ca882f0d9268a6dcbb1c9e13368e5674
eragon5779 <eragon5779@gmail.com>
2019-12-04 21:08:24 -0700

    [deleteevent] Delete uploaded file if it exists

--- deleteevent.php
+++ deleteevent.php
@@ -7,10 +7,10 @@ $userInfo = array('acct_type'=>$row['acct_type'], 'user_id' => $row['user_id']);
 
 $id = $_POST['id3'];
 
-$stmt = $db->prepare('SELECT event_contact FROM event where event_id = :id');
+$stmt = $db->prepare('SELECT event_contact, photo_path FROM event where event_id = :id');
 $stmt->execute(array(':id'=>$id));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-$itemInfo = array('event_contact'=>$row['event_contact']);
+$itemInfo = array('event_contact'=>$row['event_contact'], 'photo_path' => $row['photo_path']);
 
 if ($userInfo['acct_type'] != 2 && $userInfo['user_id'] != $itemInfo['event_contact']) {
     header("Location: index.php");
@@ -22,6 +22,12 @@ $stmt = $db->prepare('DELETE FROM eventtag where event_id = :id');
 $stmt->execute(array(':id' => $id));
 $stmt = $db->prepare('DELETE FROM event where event_id = :id');
 $stmt->execute(array(':id' => $id));
+
+if (isset($itemInfo['photo_path']))
+{
+  unlink($itemInfo['photo_path']);
+  rmdir("media/events/" . $id);
+}
 
 header('Location: index.php');
 
