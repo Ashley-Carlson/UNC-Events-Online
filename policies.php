7b1f0bda158d4c8418c0f5212b299de4063e150d
Ashley <carl7268@bears.unco.edu>
2019-12-04 21:06:53 -0700

    Policy stuff chris asked for

---
+++ policies.php
@@ -0,0 +1,48 @@
+<?php
+require_once("includes/config.php");
+require("layout/header.php");
+?>
+<h1>UNC Office of Student Organizations Policies</h1>
+<br />
+<div class="card">
+    <h2>
+			<a href="https://www.unco.edu/clubs-organizations/pdf/RSO-Manual.pdf">Registered Student Organizations Manual
+		</a>
+	</h2>
+		<br />
+		<p>This guide is intended to explain policies pertaining to Recognized Student Organizations (RSOs)
+			and describe resources that may be helpful to RSOs. Because many departments at the University of Northern Colorado
+			work with RSOs, a list of department contacts is included with this manual. Where applicable, this guide includes the
+		 name of the best department to contact for the particular service or function.
+	 </p>
+		<br />
+	</div>
+	<div class="card">
+	    <h2>
+				<a href="https://www.unco.edu/clubs-organizations/pdf/2018-2019-rso-constitution-guide.pdf">Registered Student Organizations Constitution Guide
+			</a>
+		</h2>
+			<br />
+			<p>
+				The Office of Student Clubs & Organizations at the University of Northern Colorado (UNC) requires all
+				 Recognized Student Organizations (RSOs) to submit an updated constitution each academic year. A constitution
+				 serves as the guiding document for your group and all RSOs must abide by their constitutions to maintain Active
+				 RSO status. If you’d like help with your constitution,
+				feel free to contact us at clubs@unco.edu. We’re happy to assist!
+			</p>
+			<br />
+		</div>
+		<div class="card">
+				<h2>
+					<a href="https://www.unco.edu/clubs-organizations/pdf/archiving-rso-records.pdf"> Student Group Records Transfer Form
+				</a>
+			</h2>
+				<br />
+				<p>
+					Records transferred to the University Archives are in the Archives' custody and become part of the Archives' collections.
+					Original records in the Archives' collections do not circulate. All use of original records must take place in the Archives
+					 and Special Collections Reading Room. Original documents can be loaned with advance approval. Thank you for transferring
+					 your records to the UNC University Archives! 
+					</p>
+				<br />
+			</div>
